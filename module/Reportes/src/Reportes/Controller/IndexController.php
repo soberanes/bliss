486ccf8c49216a58a5a403dc93c02e318b7d3746
Paul Soberanes <paul.soberanes@adventa.mx>
2015-04-08 18:42:26 -0500

    PLANEACIÓN #7425 - TERMINO DE REPORTES

--- module/Reportes/src/Reportes/Controller/IndexController.php
+++ module/Reportes/src/Reportes/Controller/IndexController.php
@@ -68,6 +68,7 @@ class IndexController extends AbstractActionController{
 	}
 	
 	public function cuentasAction(){
+		date_default_timezone_set("America/Mexico_City");
 		$reporte_service = $this->getServiceLocator()->get('reporte_service');
 		
 		$header = array(
@@ -89,6 +90,7 @@ class IndexController extends AbstractActionController{
 		);
 		
 		$familias = $reporte_service->getFamilias();
+
 		$x=0;
 		foreach ($familias as $familia) {
 			
@@ -118,6 +120,13 @@ class IndexController extends AbstractActionController{
 			
 			$user_data = $reporte_service->getUserData($user["user_id"]);
 			
+			$familias2 = $reporte_service->getFamilias();
+			$c_anual = 0;
+			foreach ($familias2 as $key => $value) {
+	        	$cuotas[$key] = $reporte_service->getCuotaTotalFamilia($value["familia_id"],$value["categoria"], $user["user_id"]);
+				$c_anual += $cuotas[$key];
+			}
+
 			array_push($records, 
 				array(
 			        @$user_data->distribuidor_nombre,
@@ -131,15 +140,175 @@ class IndexController extends AbstractActionController{
 			        @$user_data->estado,
 			        @$user_data->zip_code,
 			        "",
+			        @date("d/m/Y",$user_data->birthdate),
 			        $user["username"],
-			        $user["password"],
+			        $reporte_service->getUserControl($user["user_id"])->password_text,
 			        "",
-			        //ciclo por familias y cuotas!!! 
+			        $cuotas[0],
+			        $cuotas[1],
+			        $cuotas[2],
+			        $cuotas[3],
+			        $cuotas[4],
+			        $cuotas[5],
+			        $reporte_service->getCumplimiento($user["user_id"],1),
+			        $cuotas[6],
+			        $cuotas[7],
+			        $cuotas[8],
+			        $cuotas[9],
+			        $cuotas[10],
+			        $cuotas[11],
+			        $cuotas[12],
+			        $reporte_service->getCumplimiento($user["user_id"],2),
+			        // cuota anual
+			        $c_anual,
+			        // venta anual
+			        $v_anual = $reporte_service->getVentaAnual($user["user_id"]),
+			        // % cumplimiento
+			        $reporte_service->getDivisionCuota($v_anual,$c_anual),
+			        // @($v_anual*100)/$c_anual,
+			        // puntos acumulados
+			        $reporte_service->getPuntos($user["user_id"])->ganados,
+			        // puntos canjeados
+			        $reporte_service->getPuntos($user["user_id"])->canjeados,
+			        // puntos disponibles
+			        $reporte_service->getPuntos($user["user_id"])->actuales,
 			    ));
+
 		}
-	
-		$this->_predump($records);
 		
 		return $this->csvExport('master-usuarios-edo-cuenta.csv', $header, $records);
+	}
+
+	public function usuariosAction(){
+		date_default_timezone_set("America/Mexico_City");
+		$reporte_service = $this->getServiceLocator()->get('reporte_service');
+		
+		$header = array(
+				'Ocupacion',
+				'Nombre distribuidor',
+				'Puesto',
+				'Nombre completo',
+				'Email',
+				'Telefono',
+				'Celular',
+				'Domicilio',
+				'Ciudad/Municipio',
+				'Estado',
+				'Codigo postal',
+				'Referencias',
+				'Fecha de nacimiento',
+				'Usuario',
+				'Ultima sesion',
+				'Cuota anual ventas',
+				'Acumulado anual ventas',
+				'% cumplimiento',
+				'Puntos acumulados',
+				'Puntos canjeados',
+				'Puntos disponibles'
+		);
+
+		$records = array();
+		$usuarios = $reporte_service->getCuentasReport();
+
+		foreach ($usuarios as $user) {
+			$user_data = $reporte_service->getUserData($user["user_id"]);
+			
+			$familias2 = $reporte_service->getFamilias();
+			$c_anual = 0;
+			foreach ($familias2 as $key => $value) {
+	        	$cuotas[$key] = $reporte_service->getCuotaTotalFamilia($value["familia_id"],$value["categoria"], $user["user_id"]);
+				$c_anual += $cuotas[$key];
+			}
+
+			array_push($records, 
+				array(
+					"",
+			        @$user_data->distribuidor_nombre,
+			        $user["perfil"],
+			        @$user_data->fullname,
+			        @$user_data->email,
+			        @$user_data->phone,
+			        @$user_data->cellphone,
+			        @$user_data->address,
+			        @$user_data->municipio,
+			        @$user_data->estado,
+			        @$user_data->zip_code,
+			        "",
+			        @date("d/m/Y",$user_data->birthdate),
+			        $user["username"],
+			        "",
+			        $c_anual,
+			        $v_anual = $reporte_service->getVentaAnual($user["user_id"]),
+			        $reporte_service->getDivisionCuota($v_anual,$c_anual),
+			        $reporte_service->getPuntos($user["user_id"])->ganados,
+			        $reporte_service->getPuntos($user["user_id"])->canjeados,
+			        $reporte_service->getPuntos($user["user_id"])->actuales,
+		    ));
+		}
+		
+		return $this->csvExport('master-usuarios.csv', $header, $records);
+	}
+
+	public function canjesAction(){
+		date_default_timezone_set("America/Mexico_City");
+		$reporte_service = $this->getServiceLocator()->get('reporte_service');
+		$canjes_service  = $this->getServiceLocator()->get('canjes_service');
+
+		$header = array(				
+				'No. canje',
+				'Nombre del Ganador',
+				'Usuario',
+				'Id unico ganador',
+				'Codigo PMR',
+				'Codigo CU',
+				'Descripcion articulo',
+				'Cantidad',
+				'Precio articulo moneda PMR',
+				'Precio articulo (Pesos)',
+				'Domicilio entrega',
+				'CP',
+				'Referencias',
+				'Horario entrega',
+				'Nombre de la persona de entrega',
+				'Telefono de la persona de entrega',
+				'Email contacto entrega',
+				'Fecha seleccion del premio',
+				'Horario Canje',
+				'Nombre del producto'
+
+		);
+
+		$records = array();
+		$canjes = $canjes_service->canjes();
+
+		foreach ($canjes as $canje) {
+
+			array_push($records, 
+				array(
+					$canje["order_id"],
+					$canje["fullname"],
+					$canje["username"],
+					$canje["user_id"],
+					$canje["sku"],
+					"",
+					$canje["description"],
+					$canje["quantity"],
+					$canje["price"],
+					$canje["price"] * $this->pesos,
+					$canje["address"],
+					$canje["zip_code"],
+					"",
+					"",
+					$canje["fullname"],
+					$canje["phone"],
+					$canje["email"],
+					date("d/m/Y", $canje['order_date']),
+					date("H:i:s", $canje['order_date']),
+					$canje["other_sku"]
+			));
+		}
+
+		return $this->csvExport('master-canjes.csv', $header, $records);
+
 	}
 }
